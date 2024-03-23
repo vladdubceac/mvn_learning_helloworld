f7b60f3385c70cd9327499a309a72e70a84ccf49
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-03-23 22:21:55 +0200

    Added test class for surefire plugin

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("TEST - Hello ...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World !!!");
+    }
+}
